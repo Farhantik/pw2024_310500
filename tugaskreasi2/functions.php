72d4f4cab7878ebfd798be303781dc33c55cbbf1
Farhantik <farhanbima666@gmail.com>
2024-04-13 18:45:33 +0700

    membuat file baru dengan menambahkan login

---
+++ tugaskreasi2/functions.php
@@ -0,0 +1,101 @@
+<?php
+
+function koneksi()
+{
+  return  $conn = mysqli_connect('localhost', 'root', '', 'cafeshop');
+}
+
+function query($query)
+{
+  $conn  = koneksi();
+
+  $result =  mysqli_query($conn, $query);
+
+  // jika hasilnya 1 data
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return  $rows;
+}
+
+function tambah($data)
+{
+  $conn = koneksi();
+
+
+  $nama = htmlspecialchars($data['nama_menu']);
+  $harga = htmlspecialchars($data['harga_menu']);
+  $foto = htmlspecialchars($data['foto_menu']);
+
+  $query = "INSERT INTO
+              menu (nama_menu, harga_menu, foto_menu)
+            VALUES 
+            ('$nama', '$harga', '$foto');
+            ";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function hapus($id)
+{
+  $conn = koneksi();
+  mysqli_query($conn, "DELETE FROM menu  WHERE id_menu = $id") or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+  $conn = koneksi();
+
+  $id = ($data['id']);
+  $nama = htmlspecialchars($data['nama_menu'] ?? '');
+  $harga = htmlspecialchars($data['harga_menu'] ?? '');
+  $foto = htmlspecialchars($data['foto_menu'] ?? '');
+
+  $query = "UPDATE menu SET
+              nama_menu = '$nama',
+              harga_menu = '$harga',
+              foto_menu = '$foto'
+              WHERE id_menu = $id";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function cari($keyword)
+{
+  $conn = koneksi();
+  $query = "SELECT * FROM menu 
+              WHERE nama_menu LIKE '%$keyword%'";
+  $result = mysqli_query($conn, $query);
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return  $rows;
+}
+
+function login($data)
+{
+  $conn = koneksi();
+  $username = htmlspecialchars($data["username"]);
+  $password = htmlspecialchars($data["password"]);
+
+  if (query("SELECT * FROM user WHERE username = '$username' && password ='$password'")) {
+    //set session
+    $_SESSION['login'] = true;
+    header("Location: index.php");
+    exit;
+  } else {
+    return [
+      'error' => true,
+      'pesan' =>  'Username atau Password salah!'
+    ];
+  }
+}
---
+++ tugaskreasi2/index.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+if (!isset($_SESSION['login'])) {
+  header("location: login.php");
+  exit;
+}
+
+require 'functions.php';
+$menu = query("SELECT * FROM menu");
+
+// ketika tombol cari diklik
+if (isset($_POST['cari'])) {
+  $menu = cari($_POST['keyword']);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Menu</title>
+</head>
+
+<body>
+  <a href="logout.php">Logout</a>
+  <h3>Daftar Menu</h3>
+
+  <a href="tambah.php">Tambah Data Menu</a>
+  <br><br>
+
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="40" placeholder="masukkan keyword pencarian..." autocomplete="off" autofocus>
+    <button type="submit" name="cari">Cari!</button>
+  </form>
+  <br>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>foto_menu</th>
+      <th>nama_menu</th>
+      <th>harga_menu</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php if (empty($menu)) : ?>
+      <tr>
+        <td colspan="5">
+          <p style="color: red; font-style: italic;">data menu tidak ditemukan!</p>
+        </td>
+      </tr>
+    <?php endif; ?>
+
+
+    <?php $i = 1;
+    foreach ($menu as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $m['foto_menu']; ?>" width="60"></td>
+        <td><?= $m['nama_menu']; ?></td>
+        <td><?= $m['harga_menu']; ?></td>
+        <td>
+          <a href="detail.php?id=<?= $m['id_menu']; ?>">lihat detail</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+
+</body>
+
+</html>
